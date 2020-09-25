958e6845928624b3546922483e4a58447c03b72e
achyuth reddy <ncsachyuth@gmail.com>
2020-09-26 03:26:25 +0530

    getting all the countries from backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
+  // const [state, setstate] = useState(initialState)
+  const [countries, setCountries] = useState([])
+
+  useEffect(() =>{
+    const getCountriesData = async () =>{
+      await fetch ("https://disease.sh/v3/covid-19/countries")
+      .then( (response) => response.json())
+
+      .then((data) =>{
+        const countries = data.map((country) => (
+         {
+          name:country.country,
+          value:country.countryInfo.iso2
+          }
+        ))
+        setCountries(countries)
+      })
+    }
+    getCountriesData()
+  }, [countries])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Bem naming convention !!!
+    <div className="app">
+      <div className="app__header">
+        <h1>Covid 19 tracker !!!!</h1>
+          <FormControl className="app__dropdown">
+            <Select 
+              variant = "outlined"
+              value="abc"
+            >
+              {/* loop through all the countries  */}
+
+              {
+                countries.map(country =>(
+                  <MenuItem value={'country.value'}>{country.name}</MenuItem>
+                ))
+              }
+
+              {/* <MenuItem value="worldwide">WorldWide</MenuItem>
+              <MenuItem value="worldwide">option2</MenuItem>
+              <MenuItem value="worldwide">option 4 </MenuItem>
+              <MenuItem value="worldwide">Wofdfsd</MenuItem> */}
+
+            </Select>
+
+        </FormControl>
+        
+      </div>     
+      
+
+       
+
+       {/*Header  */}
+       {/* Title + select input dropdown */}
+
+       {/* infoboxes */}
+       {/* infoboxes */}
+       {/* infoboxes */}
+
+       {/* table  */}
+       {/*  Graph */}
+
+       {/* Map  */}
+
+
+
     </div>
   );
 }
