ca449deaf92078ff2fccde57cb4469a106b16dec
achyuth reddy <ncsachyuth@gmail.com>
2020-09-27 00:11:51 +0530

    table in the right side updated

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,13 +2,6 @@ export const sortData = (data) =>{
     const sortedData = [...data]
     // console.log('a.cases',a.cases);
     // sortedData.sort((a,b) => (a.cases>b.cases ? -1 : 1 ))
-    sortedData.sort((a,b) => (b.cases - a.cases ))
-        // if(a.cases>b.cases){
-        //     return -1
-        // }else{
-        //     return 1
-        // }
-    
-    return sortedData
-
+    return  sortedData.sort((a,b) => (b.cases - a.cases ))    
+   
 }
